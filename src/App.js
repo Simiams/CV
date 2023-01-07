c0868dff6970675062a63487532e2b8f6596038d
Simiams <Simon.convert@epsi.fr>
2023-01-07 16:03:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Project from "./pages/Project";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path={"/"} exact component={Home}/>
+                <Route path={"/Portfolio"} exact component={Portfolio}/>
+                <Route path={"/Portfolio/:projectName"} component={Project}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default App
